b9c2575cae8aaf55aab8882cc33ed394bb3ab825
YtBaby <736730789@qq.com>
2019-03-15 20:27:13 +0800

    commit

--- src/main/java/com/jf/datadict/Application.java
+++ src/main/java/com/jf/datadict/Application.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
-public class Application {
+public class Application  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
--- src/main/java/com/jf/datadict/config/WebMvcConfig.java
+++ src/main/java/com/jf/datadict/config/WebMvcConfig.java
@@ -8,13 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
-    /*@Override
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
         if(!registry.hasMappingForPattern("/static/**")){
             registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
         }
         super.addResourceHandlers(registry);
-    }*/
+    }
 
 }
