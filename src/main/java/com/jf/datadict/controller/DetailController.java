c31719ed3b417dd706fc68bb377a0454391586aa
736730789@qq.com <736730789@qq.com>
2019-04-15 23:44:45 +0800

    commit

--- src/main/java/com/jf/datadict/controller/DetailController.java
+++ src/main/java/com/jf/datadict/controller/DetailController.java
@@ -3,7 +3,6 @@ package com.jf.datadict.controller;
 import com.jf.datadict.model.JSONResult;
 import com.jf.datadict.service.DetailService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,9 +16,10 @@ public class DetailController {
     @Resource
     private DetailService detailService;
 
-    @GetMapping("/show")
-    public String show(HttpSession httpSession, @RequestParam("dbName") String dbName){
+    @PostMapping("/show")
+    public String show(HttpSession httpSession, @RequestParam("dbName") String dbName, @RequestParam("tableName") String tableName){
         httpSession.setAttribute("dbName", dbName);
+        httpSession.setAttribute("tableName", tableName);
         return "show";
     }
 
